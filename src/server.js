0cad3621d98977ffe1ad3d4fa1664f5fde6f0670
unknown <aminuemmazon@gmaill.com>
2024-05-21 15:42:18 +0100

    Added compression and limiting for requests

--- src/server.js
+++ src/server.js
@@ -4,12 +4,21 @@ import helmet from "helmet";
 
 import userRoutes from './user.routes.js';
 import mainRoutes from './main.routes.js';
+import ratelimit from  "express-rate-limit";
+import  compression from "compression";
 
 
 const app = express();
 const port = 3000;
 
+const limiter = ratelimit({
+    max: 100,
+    windowsMs: 60 * 1000,
+})
 
+app.use(compression())
+
+app. use(limiter); //apply rate limiting middle ware 
 app.use(express.json());
 app.use(helmet());
 
