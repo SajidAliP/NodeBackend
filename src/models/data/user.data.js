3724d8261645ff5d7ca15a572871786a8bed7045
unknown <aminuemmazon@gmaill.com>
2024-05-22 11:10:25 +0100

    Added the test scripts

--- src/models/data/user.data.js
+++ src/models/data/user.data.js
@@ -1,12 +1,12 @@
 const users = [
-    {
-        id: 1,
-        name: "Sajid Ali",
-        email: "sajid@inida.gov",
-        city: "Delhi",
-        country: "India",
-        gender: "Male",
-    }
+    // {
+    //     id: 1,
+    //     name: "Sajid Ali",
+    //     email: "sajid@inida.gov",
+    //     city: "Delhi",
+    //     country: "India",
+    //     gender: "Male",
+    // }
 
 ];
 
